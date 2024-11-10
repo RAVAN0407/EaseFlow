098263562674ff5dbabc3b645ca046c3f840ac18
Bhanu <bhanu.praneeth@paroscale.com>
2024-11-10 13:33:16 +0530

    added create/update/delete tasks methods

--- controllers/tasks.go
+++ controllers/tasks.go
@@ -9,6 +9,7 @@ import (
 	"github.com/RAVAN0407/EaseFlow/models"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func CreateTask() gin.HandlerFunc {
@@ -45,12 +46,39 @@ func UpdateTask() gin.HandlerFunc {
 		task.CreatedAt = time.Now()
 		task.UpdatedAt = time.Now()
 		task.TaskID = uuid.New()
-		result, err := taskCollection.UpdateOne(context.TODO(), task)
+		filter := bson.M{"taskid": task.TaskID}
+		result, err := taskCollection.UpdateOne(context.TODO(), filter, task)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 		c.JSON(http.StatusOK, gin.H{"successfull": result})
 
+	}
+}
+
+func DeleteTask() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		taskID := c.Param("taskId") // Assuming taskId will be passed as URL parameter
+		if taskID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "task ID is required"})
+			return
+		}
+
+		taskCollection := database.OpenConnection(database.Client, models.USER)
+		filter := bson.M{"taskid": taskID}
+
+		result, err := taskCollection.DeleteOne(context.TODO(), filter)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if result.DeletedCount == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "task not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "task deleted successfully"})
 	}
 }
